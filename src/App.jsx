ab2d8262ca455ea7e59adce65df7ea7e17910001
Гныбиденко Валерий <valeriy.gnybidenko@gmail.com>
2023-03-01 10:13:40 +0200

    feat: add eslint and prettier to template

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 function App() {
+  const name = 'hello'
+  console.log(name)
+
   return (
-    <div className='App'>
-      <h1 className='hello'>hello world</h1>
+    <div className="App">
+      <h1>Hello, its my React Template</h1>
+      <div className="hello"></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
